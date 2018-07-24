d80771b96dd3f0176bae6423fc69de3a43fce90c
a2412817347 <41611238+a2412817347@users.noreply.github.com>
2018-07-24 17:39:41 +0800

    homework
    作业1

---
+++ 作业1：数据类型.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a=[22,23,25,24,25,26,27]
+print(a[0])
+print(a[1])
+print('周三'+str(a[2]))
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[6])
